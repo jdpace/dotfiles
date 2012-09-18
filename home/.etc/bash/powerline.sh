47693e9caddf8a1c95594c80c3cfeef646eea387
Jared Pace <jared@codeword.io>
2012-09-18 09:30:47 -0400

    Link to correct version of powerline-bash

--- home/.etc/bash/powerline.sh
+++ home/.etc/bash/powerline.sh
@@ -2,7 +2,7 @@
 if [ -f ~/.etc/vendor/powerline-bash/powerline-bash.py ]; then
   function _update_ps1()
   {
-     export PS1="$(~/Dev/etc/powerline-bash/powerline-bash.py $?)"
+     export PS1="$(~/.etc/vendor/powerline-bash/powerline-bash.py $?)"
   }
 
   export PROMPT_COMMAND="_update_ps1"
