c1f75a10a201e4b30c2d5e527d9c2c384ac97b42
Jared Pace <jared@codeword.io>
2012-10-01 13:25:53 -0400

    Change ssh-autocompletion to be smarter
    Ignore missing .ssh/known_hosts and .ssh/config

--- home/.etc/bash/ssh-autocomplete.sh
+++ home/.etc/bash/ssh-autocomplete.sh
@@ -1,8 +1,17 @@
-SSH_KNOWN_HOSTS=( $(cat ~/.ssh/known_hosts | \
-  cut -f 1 -d ' ' | \
-  sed -e s/,.*//g | \
-  uniq | \
-  egrep -v [0123456789]) )
-SSH_CONFIG_HOSTS=( $(cat ~/.ssh/config | grep "Host " | grep -v "*" | cut -f 2 -d ' ') )
+if [ -f $HOME/.ssh/known_hosts ]; then
+  SSH_KNOWN_HOSTS=( $(cat ~/.ssh/known_hosts | \
+    cut -f 1 -d ' ' | \
+    sed -e s/,.*//g | \
+    uniq | \
+    egrep -v [0123456789]) )
+else
+  SSH_KNOWN_HOSTS=()
+fi
+
+if [ -f $HOME/.ssh/config ]; then
+  SSH_CONFIG_HOSTS=( $(cat ~/.ssh/config | grep "Host " | grep -v "*" | cut -f 2 -d ' ') )
+else
+  SSH_CONFIG_HOSTS=()
+fi
 
 complete -o default -W "${SSH_KNOWN_HOSTS[*]} ${SSH_CONFIG_HOSTS[*]}" ssh
