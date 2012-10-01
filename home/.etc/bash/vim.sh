854b589ae1458f543bc6dea83d9debaa625f7ccd
Jared Pace <jared@codeword.io>
2012-10-01 13:18:59 -0400

    Add Janus support to dotfiles

--- home/.etc/bash/vim.sh
+++ home/.etc/bash/vim.sh
@@ -1,5 +1,26 @@
-export VIM_APP_DIR="/usr/local/Cellar/macvim/7.3-63/"
-export EDITOR="mvim -vf"
+if [ -d /usr/local/Cellar/macvim ]; then
+  local macvim_brew_versions=(`ls /usr/local/Cellar/macvim`)
+  for latest_macvim_brew_version in "${macvim_brew_versions[@]}"; do :; done
+  if $latest_macvim_brew_version; then
+    export VIM_APP_DIR="/usr/local/Cellar/macvim/$latest_macvim_brew_version"
+  fi
+elif [ -d "/Applications/MacVim.app" ]; then
+  export VIM_APP_DIR="/Applications"
+fi
 
-alias v='mvim'
-alias vim='mvim -v'
+if [ -d $VIM_APP_DIR ]; then
+  alias v='mvim'
+  alias vim='mvim -v'
+  export EDITOR="mvim -vf"
+else
+  export EDITOR="vim -vf"
+fi
+
+
+function janus_install {
+  if [ -d $HOME/.vim ]; then
+    cd $HOME/.vim && rake
+  else
+    echo "Could not locate \$HOME/.vim"
+  fi
+}
---
+++ home/.vim
@@ -0,0 +1,1 @@
+.etc/vendor/janus
