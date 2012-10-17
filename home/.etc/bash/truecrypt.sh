c031e1983ab65fe009a83276ecb9608d18469ea3
Jared Pace <jared@codeword.io>
2012-10-17 22:59:24 +0000

    Update truecrypt helper to be more generic

--- home/.etc/bash/truecrypt.sh
+++ home/.etc/bash/truecrypt.sh
@@ -1,11 +1,21 @@
 # TrueCrypt
 if [ -f /Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt ]; then
-    function decrypt {
-      /Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt -t -k "" --protect-hidden=no ~/Documents/TrueCrypt/relevance.tc ~/Dev/relevance
-    }
-    function passwords {
-      /Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt -t -k "" --protect-hidden=no ~/Documents/TrueCrypt/passwords/quotes.txt ~/Documents/passwords
-      cd ~/Documents/passwords
-    }
-    alias tc="decrypt"
+  export TRUECRYPT=/Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt
+else
+  export TRUECRYPT=$(which truecrypt)
+fi
+
+if [ ! -z "$TRUECRYPT" ]; then
+  function decrypt {
+    local volume=$1
+    if [ -z "$volume" ] || [ ! -f "$volume" ]; then
+      echo "Please specify a volume (e.g. decrypt /path/to/volume.truecrypt)"
+      return
+    fi
+
+    local mount=$(basename "$volume" .truecrypt)
+    mkdir -p "$HOME/$mount"
+    echo "Mounting \"$volume\" to \"$HOME/$mount\""
+    $TRUECRYPT -t -k "" --protect-hidden=no $volume $mount
+  }
 fi
