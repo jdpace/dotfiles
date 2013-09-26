ad6e140f6f235b1a6b2d4d1a73285abefbbd418a
Jared Pace <jared@codeword.io>
2013-09-26 00:02:15 -0400

    Show user instead of hostname in powerline-shell

--- home/.etc/bash/powerline-shell.py
+++ home/.etc/bash/powerline-shell.py
@@ -218,30 +218,19 @@ def add_virtual_env_segment():
 add_virtual_env_segment()
 
 
-def add_hostname_segment():
-    if powerline.args.colorize_hostname:
-        from lib.color_compliment import stringToHashToColorAndOpposite
-        from lib.colortrans import rgb2short
-        from socket import gethostname
-        hostname = gethostname()
-        FG, BG = stringToHashToColorAndOpposite(hostname)
-        FG, BG = (rgb2short(*color) for color in [FG, BG])
-        host_prompt = ' %s' % hostname.split('.')[0]
 
-        powerline.append(host_prompt, FG, BG)
+def add_username_segment():
+    if powerline.args.shell == 'bash':
+        user_prompt = ' \\u '
+    elif powerline.args.shell == 'zsh':
+        user_prompt = ' %n '
     else:
-        if powerline.args.shell == 'bash':
-            host_prompt = ' \\h '
-        elif powerline.args.shell == 'zsh':
-            host_prompt = ' %m '
-        else:
-            import socket
-            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
+        import os
+        user_prompt = ' %s ' % os.getenv('USER')
 
-        powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
+    powerline.append(user_prompt, Color.USERNAME_FG, Color.USERNAME_BG)
 
-
-add_hostname_segment()
+add_username_segment()
 
 
 import os
--- home/.etc/bash/powerline.sh
+++ home/.etc/bash/powerline.sh
@@ -2,7 +2,7 @@
 if [ -f ~/.etc/bash/powerline-shell.py ]; then
   function _update_ps1()
   {
-     export PS1="$(~/.etc/bash/powerline-shell.py)"
+     export PS1="$(~/powerline-shell.py $? 2> /dev/null)"
   }
 
   export PROMPT_COMMAND="_update_ps1"
