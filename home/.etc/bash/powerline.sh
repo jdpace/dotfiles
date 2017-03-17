a580ff232944177059771916c0a7574af4353c55
Jared Pace <jared@codeword.io>
2017-03-17 09:18:26 -0400

    Don't export PLATFORM

--- home/.etc/bash/powerline.sh
+++ home/.etc/bash/powerline.sh
@@ -7,11 +7,3 @@ if [ -f ~/.etc/bash/powerline-shell.py ]; then
 
   export PROMPT_COMMAND="_update_ps1"
 fi
-
-
-# PLATFORM is needed by tmux-powerline
-if [ -z "$(uname -a | grep Darwin)" ]; then
-  export PLATFORM=linux
-else
-  export PLATFORM=mac
-fi
